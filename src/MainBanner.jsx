320293b175c43ba5b63c6bf8e857338cf76f410c
supermercado97 <isaiahmercadotx@gmail.com>
2024-12-17 13:00:23 -0600

    responsive top nav

--- src/MainBanner.jsx
+++ src/MainBanner.jsx
@@ -5,51 +5,68 @@ import './MainBanner.css';
 
 function MainBanner({ cities, profiles }) {
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false); // State for hamburger menu
     const navigate = useNavigate();
 
     const toggleDropdown = () => {
         setDropdownOpen((prev) => !prev);
     };
 
+    const toggleMenu = () => {
+        setMenuOpen((prev) => !prev);
+    };
+
     const handleCityClick = (cityId) => {
         navigate(`/cities/${cityId}`);
         setDropdownOpen(false); // Close dropdown after selection
+        setMenuOpen(false); // Close hamburger menu
     };
 
     return (
-        <div className="main-banner">
+        <div className={`main-banner ${menuOpen ? 'responsive' : ''}`}>
             <NavLink to="/" className="home-button">
                 <img alt="home icon" className="home-button-icon" src="/logo.png" />
             </NavLink>
 
-            <nav className="main-menu">
-                <NavLink to="/" className="menu-item">
-                    Home
-                </NavLink>
-                <div className="menu-item cities-dropdown" onMouseLeave={() => setDropdownOpen(false)}>
-                    <span onMouseOver={toggleDropdown}>Cities</span>
-                    {dropdownOpen && (
-                        <ul className="dropdown-menu">
-                            {cities.map((city) => (
-                                <li
-                                    key={city.name}
-                                    className="dropdown-item"
-                                    onClick={() => handleCityClick(city.id)}
-                                >
-                                    {city.name}
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-                <NavLink to="/about" className="menu-item">
-                    About
-                </NavLink>
-            </nav>
-            <SearchBar profiles={profiles}/>
-        </div>
+            {/* Hamburger Icon */}
+            <div className="hamburger-icon" onClick={toggleMenu}>
+                <i class="fa fa-bars"></i>
 
+            </div>
+
+            {/* Main Navigation */}
+            <nav className={`main-menu ${menuOpen ? 'open' : ''}`}>
+                <span id="pages-bar">
+                    <NavLink to="/" className="menu-item" onClick={() => setMenuOpen(false)}>
+                        Home
+                    </NavLink>
+                    <div className="menu-item cities-dropdown" onMouseLeave={() => setDropdownOpen(false)}>
+                        <span onMouseOver={toggleDropdown} onClick={toggleDropdown}>Cities</span>
+                        {dropdownOpen && (
+                            <ul className="dropdown-menu">
+                                {cities.map((city) => (
+                                    <li
+                                        key={city.name}
+                                        className="dropdown-item"
+                                        onClick={() => handleCityClick(city.id)}
+                                    >
+                                        {city.name}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                    <NavLink to="/about" className="menu-item" onClick={() => setMenuOpen(false)}>
+                        About
+                    </NavLink>
+                </span>
+
+
+                <SearchBar profiles={profiles} />
+            </nav>
+
+        </div>
     );
-};
+}
 
 export default MainBanner;
