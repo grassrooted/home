468f553a8084ff346f70532d7fb523fe096a45e3
supermercado97 <isaiahmercadotx@gmail.com>
2024-12-18 10:54:05 -0600

    resized headshots to square dimensions

--- src/ProfileSnapshot.js
+++ src/ProfileSnapshot.js
@@ -8,7 +8,10 @@ function ProfileSnapshot({ profile }) {
           <h1>{profile.name}</h1>
           <img className="profile-headshot" src={profile.path_to_headshot_photo} alt={`${profile.name} headshot`} />
           <h3 className='profile-district-header'>Council District {profile.district}</h3>
-          <p>First Election: {profile.first_election} | Next Election: {profile.next_election}</p>
+          <span className='row-span'>
+            <p id="first-election-wrapper">First Election <h3>{profile.first_election}</h3> </p>
+            <p id="next-election-wrapper"> Next Election <h3>{profile.next_election}</h3></p>
+          </span>
     </div>
   );
 }
