d25a073d0c5eaf6fbabcd5283dd5f37fcc99cda4
supermercado97 <isaiahmercadotx@gmail.com>
2025-01-14 15:09:07 -0600

    setting basename for router

--- src/index.js
+++ src/index.js
@@ -54,6 +54,6 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basename="/home"/>
   </React.StrictMode>
 );
