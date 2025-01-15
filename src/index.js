8d6ad2b18a27b99ae59495b058cb3952100b8d89
supermercado97 <isaiahmercadotx@gmail.com>
2025-01-14 20:46:54 -0600

    uhhh

--- src/index.js
+++ src/index.js
@@ -13,51 +13,47 @@ import {loader as cityLoader } from "./routes/City";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/home/",
-      id: "root",
-      element: <Root />,
-      errorElement: <ErrorPage />,
-      loader: rootLoader,
-      children: [
-        { 
-          index: true, 
-          element: <Main />,
-          loader: rootLoader,
-        },
-        {
-          path: "cities/:cityId",
-          element: <CityLayout />,
-          loader: rootLoader,
-          children: [
-            {
-              index: true,
-              element: <City />,
-              loader: cityLoader,
-            },
-            {
-              path: "profiles/:profileId",
-              element: <Profile />,
-              loader: profileLoader,
-            },
-          ],
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-      ],
-    },
-  ],
-  { basename: "/home" } // Correct placement of basename
-);
-
+const router = createBrowserRouter([
+  {
+    path: "/home/",
+    id: "root",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      { 
+        index: true, 
+        element: <Main />,
+        loader: rootLoader
+      },
+      {
+        path: "cities/:cityId",
+        element: <CityLayout />,
+        loader: rootLoader,
+        children: [
+          {
+            index: true,
+            element: <City />,
+            loader: cityLoader,
+          },
+          {
+            path: "profiles/:profileId",
+            element: <Profile />,
+            loader: profileLoader,
+          },
+        ],
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ],
+  },
+]);
 
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} basename="/home" />
   </React.StrictMode>
 );
